7bbcaa15871bafd175518aef0c299d408aaf8c11
tm00mt <3.your.mind@gmail.com>
2013-03-17 12:05:31 +0100

    Kolorowe klocki, dzwiek podczas kasowania linii
    Klocki wreszcie są kolorowe; kolory są losowane z kilku dostępnych
    Wstawiłem też jakiś dźwięk, który odtwarzany jest przy kasowaniu
    całego wiersza (wierszy)

--- TetrisMove/gen/pl/tmsj/tetrismove/R.java
+++ TetrisMove/gen/pl/tmsj/tetrismove/R.java
@@ -14,34 +14,37 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070001;
-        public static final int player_name=0x7f070000;
+        public static final int menu_settings=0x7f080001;
+        public static final int player_name=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int alert_dialog_text_entry=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
+    }
+    public static final class raw {
+        public static final int bomb=0x7f040000;
     }
     public static final class string {
-        public static final int Cancel=0x7f040008;
-        public static final int GameOver=0x7f040001;
-        public static final int Gratz=0x7f040005;
-        public static final int Lost=0x7f040002;
-        public static final int Next=0x7f04000c;
-        public static final int Pause=0x7f040009;
-        public static final int Quit=0x7f04000f;
-        public static final int QuitG=0x7f040004;
-        public static final int Replay=0x7f040003;
-        public static final int Restart=0x7f04000b;
-        public static final int Resume=0x7f04000a;
-        public static final int SaveS=0x7f040007;
-        public static final int Score=0x7f04000d;
-        public static final int TopScores=0x7f04000e;
-        public static final int Win=0x7f040006;
-        public static final int app_name=0x7f040000;
-        public static final int menu_settings=0x7f040010;
+        public static final int Cancel=0x7f050008;
+        public static final int GameOver=0x7f050001;
+        public static final int Gratz=0x7f050005;
+        public static final int Lost=0x7f050002;
+        public static final int Next=0x7f05000c;
+        public static final int Pause=0x7f050009;
+        public static final int Quit=0x7f05000f;
+        public static final int QuitG=0x7f050004;
+        public static final int Replay=0x7f050003;
+        public static final int Restart=0x7f05000b;
+        public static final int Resume=0x7f05000a;
+        public static final int SaveS=0x7f050007;
+        public static final int Score=0x7f05000d;
+        public static final int TopScores=0x7f05000e;
+        public static final int Win=0x7f050006;
+        public static final int app_name=0x7f050000;
+        public static final int menu_settings=0x7f050010;
     }
     public static final class style {
         /** 
@@ -65,10 +68,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
